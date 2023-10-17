3d504e570a2ee47f0a7015fae35eb02c3511af88
Madan Y N - 200070040@iitb.ac.in <madanyn@vlsi35.ee.iitb.ac.in>
2023-10-17 20:24:31 +0530

    files upload

---
+++ caches.py
@@ -0,0 +1,44 @@
+#Cache Architecture
+from m5.objects import Cache
+
+#Base class for creating L1 I & D caches
+#Note : Read the Cache class to understand the member fields and methods
+class L1Cache(Cache):
+        assoc = 2 #two-way set associvity
+        tag_latency = 2 #cache hit latency
+        data_latency = 2
+        response_latency = 2
+        mshrs = 4
+        tgts_per_mshr = 20
+
+        def connectCPU(self, cpu): #raises if connectCPU is not implemented in child classes
+                raise NotImplementedError
+
+        def connectBus(self, bus): #connects mem_size port of both i and d caches to membus
+                self.mem_side = bus.cpu_side_ports
+
+
+class L1ICache(L1Cache):
+        size = '16kB'
+
+        def connectCPU(self, cpu):
+                self.cpu_side = cpu.icache_port
+
+class L1DCache(L1Cache):
+	size = '64kB'
+	def connectCPU(self, cpu):
+		self.cpu_side = cpu.dcache_port
+class L2Cache(Cache):
+	size = '256kB'
+	assoc = 8
+	tag_latency = 20
+	data_latency = 20
+	response_latency = 20
+	mshrs = 20
+	tgts_per_mshr = 12
+
+	def connectCPUSideBus(self, bus):
+		self.cpu_side = bus.mem_side_ports
+
+	def connectMemSideBus(self, bus):
+		self.mem_side = bus.cpu_side_ports
