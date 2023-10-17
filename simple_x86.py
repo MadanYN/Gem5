cff86019a68942d09d643716b2476aaacddac5a7
MadanYN <madanyn3@gmail.com>
2023-10-17 20:01:42 +0530

    Update simple_x86.py

--- simple_x86.py
+++ simple_x86.py
@@ -4,6 +4,20 @@ import m5
 from m5.objects import *
 from caches import *
 
+import argparse
+
+parser = argparse.ArgumentParser(description='A simple system with 2-level cache.')
+parser.add_argument("binary", default="", nargs="?", type=str,
+                    help="Path to the binary to execute.")
+parser.add_argument("--l1i_size",
+                    help=f"L1 instruction cache size. Default: 16kB.")
+parser.add_argument("--l1d_size",
+                    help="L1 data cache size. Default: Default: 64kB.")
+parser.add_argument("--l2_size",
+                    help="L2 cache size. Default: 256kB.")
+
+options = parser.parse_args()
+
 system = System() #System() class contains all info about the system being simuated
 system.clk_domain = SrcClockDomain()
 system.clk_domain.clock = '1GHz'
@@ -52,8 +66,10 @@ system.mem_ctrl.dram = DDR3_1600_8x8()
 system.mem_ctrl.dram.range = system.mem_ranges[0]
 system.mem_ctrl.port = system.membus.mem_side_ports
 
-binary = 'tests/test-progs/hello/bin/x86/linux/hello'
-system.workload = SEWorkload.init_compatible(binary)
+# binary = 'tests/test-progs/hello/bin/x86/linux/hello'
+# system.workload = SEWorkload.init_compatible(binary)
+
+system.workload = SEWorkload.init_compatible(options.binary)
 
 process = Process()
 process.cmd = [binary]
