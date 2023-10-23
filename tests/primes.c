2497b8f994adb87b1008d32ab0472e75799d6003
MadanYN <madanyn3@gmail.com>
2023-10-24 00:33:23 +0530

    primes testbench

---
+++ tests/primes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define N 1000000000
+
+int main(){
+    // bool isPrime[N+1];
+    bool *isPrime = (bool*)malloc((N+1) * sizeof(bool));
+    if (isPrime == NULL) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+    for(int i = 2; i <= N ; i++){
+        isPrime[i] = true;
+    }
+
+    for(int p = 2; p*p <= N; p++){
+        if(isPrime[p] == true){
+            for(int i = p*p; i <= N; i += p){
+                isPrime[i] = false;
+            }
+        }
+    }
+    long count = 0;
+    for(int i = 2; i <= N; i++){
+        if(isPrime[i]){
+            count++;
+        }
+    }
+
+    printf("Number of primes less than %d is %ld\n", N, count);
+}
